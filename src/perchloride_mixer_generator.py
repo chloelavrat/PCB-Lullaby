46e01115df651612f0e913162490f02e9ae6e4c9
Azerty-Labs <75258525+azerty-labs@users.noreply.github.com>
2024-06-20 12:17:45 +0300

    mixer optimizations

--- src/perchloride_mixer_generator.py
+++ src/perchloride_mixer_generator.py
@@ -1,45 +1,45 @@
-## --------------------------------------------
-## Title : Perchloride mixer gcode
-## Author : C.Lavrat
-## Date : 2021/01/23
-## -----------------------------------------------------------------------
+"""
+Title  : Perchloride mixer gcode
+Author : C.Lavrat
+Date   : 2021/01/23
+"""
+def process(filename: str, zPosition: int, numWait: int, bedTemp: int, backPosition: int, frontPosition: int, speed: int, timeMix: float) -> None:
+    """
+    Processes the PCB mixing operation and generates G-code instructions.
 
-def process(fileName, zPosition, numWait, bedTemp, backPosition, frontPosition, speed, timeMix):
-    with open(fileName, 'w') as file:
+    Args:
+        filename: The name of the file to write the G-code instructions to.
+        zPosition: The Z-axis position for the PCB.
+        numWait: The number of waiting cycles.
+        bedTemp: The temperature for heating the bed.
+        backPosition: The back position for the PCB.
+        frontPosition: The front position for the PCB.
+        speed: The speed of the PCB movement.
+        timeMix: The time duration for mixing.
+
+    Returns:
+        None
+    """
+    with open(filename, 'w') as file:
         ## -- Place plate
-        file.write("G28\n") # Move the X,Y,Z to absolute position
-        file.write("G90\n") # Absolute programming in relation to the program origin
-        file.write(f"G0 Z{int(zPosition)}" + "\n")
+        file.write("G28\nG90\n")
+        file.write(f"G0 Z{zPosition}\n")
         for i in range(numWait):
-            file.write(f"M117 Place Circuit NOW! {str(numWait - i)}" + "\nG4 P1000\n")
+            file.write(f"M117 Place Circuit NOW! {numWait - i}\nG4 P1000\n")
         file.write("M117 Circuit Rocking G-Code\nG0 Z200\n")
         ## -- init zero
-        file.write("G28 X0 Y0\n") # move to X=0 Y=0
-        ## -- stepper motors
-        file.write("M18\n") # M18: Disable all stepper motors to save energie
+        file.write("G28 X0 Y0\nM18\n")
         if bedTemp != 0:
-            file.write(f"M117 Heating Bed To {int(bedTemp)}" + "°C\n")
-            file.write(f"M190 S{int(bedTemp)}" + "\n")
+            file.write(f"M117 Heating Bed To {bedTemp}°C\nM190 S{bedTemp}\n")
         ## -- start Rocking
-        file.write("G4 P1000\n")
-        file.write("M117 Rocking the PCB..\n")
-        t = ((backPosition-frontPosition)/speed)
-        timeMix = int((timeMix*60)/t)
+        file.write("G4 P1000\nM117 Rocking the PCB..\n")
+        
+        t = (backPosition - frontPosition) / (speed if speed != 0 else 100)
+        timeMix = int((timeMix * 60) / t)
+        
         for _ in range(timeMix):
-            file.write(
-                (
-                    (
-                        (
-                            (f"G1 Y{int(backPosition)} F{int(speed)}" + "\n")
-                            + "G1 Y"
-                        )
-                        + str(int(frontPosition))
-                        + " F"
-                    )
-                    + str(int(speed))
-                    + "\n"
-                )
-            )
+            file.write(f"G1 Y{backPosition} F{speed}\nG1 Y{frontPosition} F{speed}\n")
+
 
 
 if __name__ == "__main__":
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Perchloride mixer gcode')
     parser.add_argument('filename', metavar='FILE', type=str, help='name of the gcode file')
     parser.add_argument('--bedTemp', type=int, default=30, help='bed temperature (default: 30)')
-    parser.add_argument('--timeMix', type=int, default=1, help='duration of the mix, in minutes (default: 1)')
+    parser.add_argument('--timeMix', type=float, default=1, help='duration of the mix, in minutes (default: 1)')
     parser.add_argument('--numWait', type=int, default=2, help='number of messages at start ("Place Circuit NOW!") (default: 2)')
     parser.add_argument('--speed', type=int, default=2000, help='speed of the bed (default: 2000)')
     parser.add_argument('--zPosition', type=int, default=200, help='Z position to free space for the glass container (default: 200)')
