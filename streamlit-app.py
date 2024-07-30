5043956231f17acc23d087d2373ccba66685d6b9
Azerty-Labs <75258525+azerty-labs@users.noreply.github.com>
2024-07-30 15:08:29 +0200

    add banner + rename page

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import subprocess
 
 # Function to execute the command and generate the output file
+
+
 def generate_gcode(file_name, bedTemp, timeMix, numWait, speed, zPosition, frontPosition, backPosition):
     command = [
         'python', './src/perchloride_mixer_generator.py',
@@ -24,12 +26,13 @@ def generate_gcode(file_name, bedTemp, timeMix, numWait, speed, zPosition, front
             gcode = file.read()
         return gcode
 
-st.title("PCB EtchCode G-code Creator")
+
+st.title("PCB Lullaby")
+st.subheader("Achieve faster PCB etching with your 3D printer ! ")
+st.image("banner.png")
 st.markdown("""
-            Welcome to the PCB EtchCode G-code Creator app! 
-            
-            This application is designed to simplify the process of generating G-code for controlling your Ender3 3D printer to mix iron perchloride for PCB etching. By utilizing the heated bed and (x,y) plate movements of the Ender 3, this app ensures efficient mixing of the fluid, significantly speeding up the PCB manufacturing process.
-            
+            This application is designed to streamline the process of generating G-code for controlling your Ender 3 Pro 3D printer to mix iron perchloride for PCB etching. By utilizing the heated bed and (x,y) plate movements of the Ender 3, this app ensures efficient mixing of the fluid, significantly speeding up the PCB manufacturing process.
+
             Fill in the parameters, and click "Generate the G-code" to generate and download the customized G-code for your setup.""")
 
 with st.expander("**Demo video**"):
@@ -41,7 +44,8 @@ gcode_content = None
 with st.form("my_form"):
     st.write("**Fill in the parameters below to adapt the pattern**")
     file_name_input = st.text_input("File name", "EtchCode_output.gcode")
-    bedTemp_input = st.number_input("Bed temperature (0°C if no heat needed)", value=30)
+    bedTemp_input = st.number_input(
+        "Bed temperature (0°C if no heat needed)", value=30)
     timeMix_input = st.number_input("Mix duration (minutes)", value=1)
     numWait_input = st.number_input("Number of messages at start ", value=2)
     speed_input = st.number_input("Bed speed", value=2000)
@@ -50,7 +54,9 @@ with st.form("my_form"):
     zPosition_input = st.number_input("Z position safe", value=200)
 
     if submitted := st.form_submit_button("Generate the G-code"):
-        gcode_content = generate_gcode(file_name_input, bedTemp_input, timeMix_input, numWait_input, speed_input, zPosition_input, frontPosition_input, backPosition_input)
+        gcode_content = generate_gcode(file_name_input, bedTemp_input, timeMix_input, numWait_input,
+                                       speed_input, zPosition_input, frontPosition_input, backPosition_input)
 
 if gcode_content:
-    st.download_button(label="Download G-code", data=gcode_content, file_name=file_name_input, mime="text/plain")
+    st.download_button(label="Download G-code", data=gcode_content,
+                       file_name=file_name_input, mime="text/plain")
